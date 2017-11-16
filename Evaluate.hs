f1d2a7a0df315fb97c8c294efab6ddddc70d1547
dominic <dom.orchard@gmail.com>
2017-11-16 00:18:15 +0000

    make function argument amount the top-level parameter

--- Evaluate.hs
+++ Evaluate.hs
@@ -14,9 +14,9 @@ main :: IO ()
 main = do
   -- Header
   putStrLn "#funs   #funLen   #args   single   multi"
-  forM_ [10,20..40] $ \funs ->
-   forM_ [10,20..40] $ \funLen ->
-    forM_ [2,4..8] $ \funArgs -> do
+  forM_ [2,4..8] $ \funArgs ->
+   forM_ [10,20..40] $ \funs ->
+    forM_ [10,20..40] $ \funLen -> do
       -- 1. Synthesise the whole program...
       bigSynthesise "whole" "big" funs funLen funArgs
       -- ... infer its units, recording the time
