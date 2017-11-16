1ea30832b71795b996ae4193341489dff8000db8
dominic <dom.orchard@gmail.com>
2017-11-16 23:42:44 +0000

    time navier whole

---
+++ timeNavierWhole.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import System.Process (system)
+import Control.Monad (replicateM)
+import System.Clock
+
+main :: IO ()
+main = do
+  -- Header
+  file1 <- timeProcess "camfort" ["units-compile", "navierWhole.f90"]
+  putStrLn (show $ file1)
+
+timeProcess :: String -> [String] -> IO Double
+timeProcess c args =
+   replicateM 3 timing >>= (\times -> return $ sum times / 3.0)
+  where
+   timing = do
+     start <- getTime Monotonic
+     _     <- system (c ++ " " ++ unwords args ++ " >> log 2>&1")
+     end   <- getTime Monotonic
+     return $ fromIntegral (toNanoSecs (diffTimeSpec end start)) / (10^(9 :: Integer)::Double)
